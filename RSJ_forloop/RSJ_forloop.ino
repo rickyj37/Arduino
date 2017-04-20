edcea60df8f233191d533e9d1c64dd4284800fca
rickyj37 <rickyjohnson37@gmail.com>
2017-04-20 10:19:30 -0700

    Add files via upload

---
+++ RSJ_forloop/RSJ_forloop.ino
@@ -0,0 +1,90 @@
+// RGBW (Red Green Blue White Neo-Pixel starter code
+// 16 LEDS  increment
+// CW Coleman 170413
+int clearColor();
+int redColor();
+int greenColor;
+int blueColor();
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#define PIN 6
+
+#define NUM_LEDS 16
+
+#define BRIGHTNESS 50
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800);
+
+
+void setup() {
+  Serial.begin(115200);
+  strip.setBrightness(BRIGHTNESS);
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+// Initialize some variables for the void loop()
+
+
+void loop() {
+  clearColor();
+  redColor();
+
+  
+}
+  
+int clearColor(){
+  int red = 1, green = 0, blue = 0, white = 0;
+int wait = 500;
+int led = 0;
+int i;
+   for ( led = 0; led < 16; led++){  
+    strip.setPixelColor(led, red, green , blue, white);
+  }//end of for loop
+    strip.show();
+    delay(wait);
+    red = red + 9;
+    green = green + 15;
+    blue = blue + 32;
+    white = white + 1;
+
+    if (red > 255) red = 0;
+    if (green > 255) green = 0;
+    if (blue > 255) blue = 0;
+    if (white > 50) white = 0;
+       
+    if (red < 0) red = 255;
+    if (green  < 0) green = 255;
+    if (blue < 0) blue = 255;
+    if (white  < 0 ) white = 50;
+}
+int redColor(){
+  int red = 255, green = 0, blue = 0, white = 50;
+int wait = 500;
+int led = 0;
+int i;
+   for ( led = 0; led < 16; led++){  
+    strip.setPixelColor(led, red, green , blue, white);
+  }//end of for loop
+    strip.show();
+    delay(wait);
+    red = red + 9;
+    green = green + 15;
+    blue = blue + 32;
+    white = white + 1;
+
+    if (red > 255) red = 0;
+    if (green > 255) green = 0;
+    if (blue > 255) blue = 0;
+    if (white > 50) white = 0;
+       
+    if (red < 0) red = 255;
+    if (green  < 0) green = 255;
+    if (blue < 0) blue = 255;
+    if (white  < 0 ) white = 50;
+}
+
+
